1f663e976d11c1378182c9217cfeba0c3e5501ed
sorker3 <sorker4@hotmail.com>
2018-01-12 11:43:37 +0100

    Command parsing

---
+++ index.ts
@@ -0,0 +1,9 @@
+const args = require('command-line-args');
+
+const argDef = [
+    {name: 'msg', type:String, multiple: true, defaultOption: true}
+];
+
+const Parser = args(argDef);
+
+console.log(Parser.msg);
