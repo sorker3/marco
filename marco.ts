90c1687fa409bbe638c9039ff30d54850616413c
sorker3 <sorker4@hotmail.com>
2018-01-13 12:53:13 +0100

    Begun testing args, testing client structure

--- marco.ts
+++ marco.ts
@@ -3,7 +3,25 @@ const clargs = require('commander');
 
 clargs 
     .version('0.0.4')
+
+    .option('-c, --client', 'Run as client')
+
     .parse(process.argv);
+
+
+class client {
+    connect(ip:any, port:number) {
+        console.log('Connetcting to IP %s using port %j', ip, port);
+    }
+
+    send(data:string) {
+        console.log(data);
+    }
+
+    close() {
+        console.log('Closing connection');
+    }
+}
 
 
 console.log(clargs.args.join(' '));
