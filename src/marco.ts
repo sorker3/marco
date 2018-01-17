316f80286b65af484c7e61a27517bcc2cd49593f
sorker3 <sorker4@hotmail.com>
2018-01-17 23:02:59 +0100

    Still testing commander

--- src/marco.ts
+++ src/marco.ts
@@ -4,10 +4,8 @@ const clargs = require('commander');
 //Handle arguments
 clargs 
     .version('0.0.4')
-
-    .option('-c, --client', 'Run as client')
-
+    .option('-c, --client <n>', 'Run as client', parseInt)
     .parse(process.argv);
 
-
-console.log(clargs.args.join(' '));
+if (clargs.client) console.log('Running as client at ' + clargs.integer);
+if (clargs.args) console.log(clargs.args.join(' '));
