5b861d52c903e62ac6b6f7b6e26637c7862a048d
sorker3 <sorker4@hotmail.com>
2018-02-04 01:51:27 +0100

    Testing a 'server' command

--- src/marco.ts
+++ src/marco.ts
@@ -1,3 +1,25 @@
 const clargs = require('subcommander');
 
+
+var srv = clargs.command('server', {
+    desc: 'Starts a marco server',
+    callback: function(options) {
+            var port = options.port;
+            var hostname = options.hostname;
+
+            console.log(port, hostname);
+    }
+    })
+    .option('port', {
+        abbr: 'p',
+        desc: 'Server port',
+        default: 8080
+    })
+    .option('hostname', {
+        abbr: 'H',
+        desc: 'Server hostname'
+    });
+
+clargs.parse();
+
 console.log(clargs.parse());
