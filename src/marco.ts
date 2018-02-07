cec2d451a65cfbebe04f329967ce0e485321f1b6
sorker3 <sorker4@hotmail.com>
2018-02-07 21:45:14 +0100

    Added client command

--- src/marco.ts
+++ src/marco.ts
@@ -1,27 +1,53 @@
 const clargs = require('subcommander');
 
 
-clargs.command('server', {
-    desc: 'Starts a marco server',
-    callback: function ( options ) {
-        var port = options.port,
-            hostname = options.hostname;
- 
-        console.log( port, hostname );
-    }
-    })
-        .option('port', {
-            abbr: 'p',
-            desc: 'Server port',
-            default: 8080
+clargs
+    .command('server', {
+        desc: 'Starts a marco server',
+        callback: function (options) {
+            let port:number = options.port,
+                host:string = options.hostname;
+        
+            console.log(port, host);
+        }
         })
-        .option('hostname', {
-            abbr: 'H',
-            desc: 'Server hostname',
-            default: 'localhost'
+            .option('port', {
+                abbr: 'p',
+                desc: 'Server port',
+                default: 8080
+            })
+            .option('hostname', {
+                abbr: 'H',
+                desc: 'Server hostname',
+                default: 'localhost'
+            })
+        .end()
+    .command('client', {
+        desc: 'Use as a client',
+        callback: function (options) {
+            let port:number = options.port,
+                host:string = options.hostname,
+                msg:string = options.message;
+        
+            console.log(port, host, msg);
+        }
         })
-    .end();
-
+            .option('hostname', {
+                abbr: 'H',
+                desc: 'Server hostname',
+                default: 'localhost'
+            })
+            .option('port', {
+                abbr: 'p',
+                desc: 'Server port',
+                default: 8080
+            })
+            .option('message', {
+                abbr: 'm',
+                desc: 'Message to send to server',
+                default: 'Marco... Polo!'
+            })
+        .end();
 clargs.parse();
 
 console.log(clargs.parse());
